9c0035db3e4cc305aa0cb9a4b68c9a5adb14ece0
Ace Baum <jaybaum.03@gmail.com>
2026-05-10 17:52:25 -0400

    update index to match sample

--- index.js
+++ index.js
@@ -8,40 +8,34 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// TODO: Init with env variable
 const PRIVATE_APP_ACCESS = process.env.ACCESS_TOKEN;
 
-const headers = {
-  Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-  'Content-Type': 'application/json'
-}
-
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
 app.get('/', async (req, res) => {
-  // TODO: Implement homepage
-  console.log("Hello from homepage...");
-  const resp = await axios.get('https://api.hubapi.com/crm/v3/objects/companies?properties=name,domain,city,age,color,activity_status', { headers });
-  res.render('homepage', { data: resp.data.results });
+  const companies = 'https://api.hubapi.com/crm/v3/objects/companies?properties=name,domain,city,age,color,activity_status'
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  }
+
+  try {
+    const resp = await axios.get(companies, { headers });
+    const data = resp.data.results;
+    res.render('homepage', { title: 'Homepage | Integrating With HubSpot I Practicum', data });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
 app.get('/update-cobj', async (req, res) => {
-  // TODO: Implement homepage
-  console.log("Hello from get update-cobj...");
-  res.render('updates');
+  try {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
 app.post('/update-cobj', async (req, res) => {
-  // TODO: Implement homepage
-  console.log("Hello from post update-cobj...");
-  const newCompany = {
+  const update = {
     properties: {
       "name": req.body.newName,
       "city": req.body.newCity,
@@ -51,54 +45,20 @@ app.post('/update-cobj', async (req, res) => {
       "activity_status": req.body.statuses
     }
   }
-  const resp = await axios.post('https://api.hubapi.com/crm/v3/objects/companies', newCompany, { headers });
-  res.redirect('/');
+
+  const companies = 'https://api.hubapi.com/crm/v3/objects/companies'
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  }
+
+  try {
+    await axios.post(companies, update, { headers });
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+  }
 });
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
-});
-*/
-
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
